e1825a0570272e41ec191285ce5eafc5f48fbc11
Shakti-Hexate <108566945+Shakti-Hexate@users.noreply.github.com>
2025-01-02 13:22:48 +0530

    Delete FYERS_Trading_engine directory

--- FYERS_Trading_engine/src/WriteCallbackcpp.cpp
+++
@@ -1,6 +0,0 @@
-#include <string>
-
-size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* userp) {
-    userp->append((char*)contents, size * nmemb);
-    return size * nmemb;
-}
